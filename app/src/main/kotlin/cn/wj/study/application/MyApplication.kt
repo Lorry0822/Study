6a8b7b9ef567d32ffc80fdd61fed6b4257845845
王杰 <15555650921@163.COM>
2019-05-16 16:59:37 +0800

    项目创建

---
+++ app/src/main/kotlin/cn/wj/study/application/MyApplication.kt
@@ -0,0 +1,14 @@
+package cn.wj.study.application
+
+import android.app.Application
+
+/**
+ * 应用全局对象
+ */
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
